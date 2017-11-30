a6f747651db2fe71790fac15afe25be4052a5e2c
PRIYANKA ARORA <priyankarr140@gmail.com>
2017-11-30 22:52:16 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+ <script>
+        $(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 2000 } );
+            $("#carousel-button").click(function(){
+                if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+                    $("#mycarousel").carousel('pause');
+                    $("#carousel-button").children("span").removeClass('fa-pause');
+                    $("#carousel-button").children("span").addClass('fa-play');
+                }
+                else if ($("#carousel-button").children("span").hasClass('fa-play')){
+                    $("#mycarousel").carousel('cycle');
+                    $("#carousel-button").children("span").removeClass('fa-play');
+                    $("#carousel-button").children("span").addClass('fa-pause');                    
+                }
+            });
+            $("#carousel-play").click(function(){
+                $("#mycarousel").carousel('cycle');
+            });
+        });
+    </script>
